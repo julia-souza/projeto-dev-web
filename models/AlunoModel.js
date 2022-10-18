fcf2ab9019761e52a9765cbef6e753cc0447a82f
Júlia Souza <juliadsouzaborges@gmail.com>
2022-10-18 18:05:22 -0300

    correção AlunoModel

--- models/AlunoModel.js
+++ models/AlunoModel.js
@@ -24,44 +24,7 @@ const AlunoSchema = new Schema(
             unique: true,
             index: true,
         },
-        password: {
-            type: String,
-            required: true,
-        },
-        ativo: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    { timestamps: true }
-);
-export default mongoose.model("Aluno", AlunoSchema);import mongoose from "mongoose";
-const { Schema } = mongoose;
-const AlunoSchema = new Schema(
-    {
-        nome: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        dataNascimento: {
-            type: Date,
-            required: true,
-        },
-        sexo: {
-            type: String,
-            required: true,
-            trim: true,
-            enum: ["M", "F"],
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-            index: true,
-        },
-        password: {
+        senha: {
             type: String,
             required: true,
         },
